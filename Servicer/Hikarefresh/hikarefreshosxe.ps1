eee6ed64d8d291e4f9d86c43777084d997cb257f
Bionic Butter <44027930+Bionic-OSE@users.noreply.github.com>
2024-01-12 22:58:25 +0700

    22111.101
    Vendor layer updater quick fix

--- Servicer/Hikarefresh/hikarefreshosxe.ps1
+++ Servicer/Hikarefresh/hikarefreshosxe.ps1
@@ -8,12 +8,14 @@ function Show-Branding {
 }
 
 Show-Branding
-Write-Host "Updating your image (Stage 3)" -ForegroundColor White; Write-Host " "
+Write-Host "Updating user experience layer" -ForegroundColor White; Write-Host " "
 
 Stop-Process -Name HikaruQML
 Start-Sleep -Seconds 3
 Remove-Item -Path $env:SYSTEMDRIVE\Bionic\Hikaroste -Force -Recurse
-Start-Process 7za -Wait -NoNewWindow -ArgumentList "x $env:SYSTEMDRIVE\Bionic\Hikarefresh\Delivery\Vendor.7z -pBioniDKU -o$env:SYSTEMDRIVE\Bionic -aoa"
+Start-Process 7za -Wait -NoNewWindow -ArgumentList "x $env:SYSTEMDRIVE\Bionic\Hikarefresh\Delivery\Vendor.7z -pBioniDKU -o$env:SYSTEMDRIVE\Bionic\Hikarefresh\Delivery\Vendor -aoa"
 & $env:SYSTEMDRIVE\Bionic\Hikarefresh\Delivery\Vendor\Imagervicing.ps1
+. $env:SYSTEMDRIVE\Bionic\Hikarefresh\Versinfo.ps1
+Set-ItemProperty -Path "HKCU:\Software\Hikaru-chan" -Name "Revision" -Value "24010.$version" -Force
 Write-Host " "; Write-Host "Update completed!" -ForegroundColor Black -BackgroundColor White
 Start-Sleep -Seconds 5
